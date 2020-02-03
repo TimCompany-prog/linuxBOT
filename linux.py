4493f14c177f3de9eb9e0c9ec802578cd3a0f70f
timoha <youtimoha@gmail.com>
2020-02-03 16:56:15 +0200

    upg

--- linux.py
+++ linux.py
@@ -9,10 +9,8 @@ a = types.ReplyKeyboardMarkup(resize_keyboard=True)
 a1 = types.KeyboardButton("включи бойлер")
 a2 = types.KeyboardButton("виключи бойлер")
 a.add(a1,a2)
-#o = str(random.randint(1, 10))
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    #bot.send_message(message.chat.id, 'Привіт я бот тімоха хочеш дізнатися команди нажми на /help')
     bot.send_message(-1001495083425, 'УВАГА!!!УВАГА!!!УВАГА!!!\nВ бота добавилася нова функція якщо ти напишеш\n/russianroulette\nто у тебе є шанс 1 із 10 що ти получиш мут на день пробуй)\nмут - незмога писати повідомлень')
 @bot.message_handler(commands=['help'])
 def start_message(message):
@@ -22,55 +20,21 @@ def start_message(message):
 def send_text1(message):
     o = str(random.randint(1, 10))
     bot.restrict_chat_member(message.chat.id,message.from_user.id, until_date=time()+120)
-
     if message.chat.type == 'supergroup':
          if message.chat.title == 'Линуксоиды':
-
-
              if o == '1':
                  bot.restrict_chat_member(message.chat.id,message.from_user.id, until_date=time()+86400)
                  bot.send_message(-1001495083425, 'мут на день) твоє число: ' + '1')
-
              if o != '1':
-
                  print(o)
-
                  bot.send_message(-1001495083425, 'ти везунчик тебе випало: ' + o)
-
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-
     if message.chat.type == 'supergroup':
          if message.chat.title == 'Линуксоиды':
              if message.text.lower() == 'BOOM':
-
-
-
-        
-
-        
-                 bot.restrict_chat_member(message.chat.id,message.from_user.id, until_date=time()+120)
-             
-
-                    
-
+                 bot.restrict_chat_member(message.chat.id,message.from_user.id, until_date=time()+20000)
              if message.text.lower() == 'привет':
 
-                 bot.send_message(-1001495083425, 'привет ')
-    
-
-
-     
-    #if message.text.lower() == 'на якій мові ти написаний':
-    #    bot.send_message(message.chat.id, 'на python')
-   # if message.text.lower() == 'привіт':
-   #     pass
-        #bot.restrict_chat_member(-1001461255585,, until_date=time()+120)
-        #bot.send_message(-1001485207430, "привіт всім")
-        #bot.send_message(-1001461255585, "привіт сімʼя")
-    #if message.text.lower() == 'хто розробник':
-    #    bot.send_message(message.chat.id, 'Мій розробник тімоха @timproger')
-    #if message.text.lower() == 'бувай':
-    #    bot.send_message(message.chat.id, 'допобачення люди')
-
+                 bot.send_message(-1001495083425, 'привет ')  
 bot.polling(none_stop=True)
